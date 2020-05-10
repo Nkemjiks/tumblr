c12de36ea3ef60cee3d00844c268eb19769a2bd2
Blessing Mbonu <mbonunkemjika@ymail.com>
2020-05-10 15:46:18 +0100

    More html and validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  validates :title, presence: { message: "can't be blank" }, length: { minimum: 3 }
+  validates :content, presence: { message: "can't be blank" }, length: { minimum: 5 }
 end
