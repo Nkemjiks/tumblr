d3746793db8a23e157c94defa93a272ce8757781
Blessing Mbonu <mbonunkemjika@ymail.com>
2020-05-11 12:54:42 +0100

    Add user, link post to user, add restrictions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,13 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_post, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all.order("created_at DESC")
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -16,7 +18,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.new(post_params)
 
     respond_to do |format|
       if @post.save
@@ -52,7 +54,7 @@ class PostsController < ApplicationController
   private
 
   def set_post
-    @post = Post.find(params[:id])
+    @post = current_user.posts.find(params[:id])
   end
 
   def post_params
